0df736c0d0c143b1728944322c694f510948a15d
Matias Cayul <ma.cayul@duocuc.cl>
2026-08-31 17:38:40 -0400

    Se agrego interactividad al metodo crear informe

--- assets/js/historial-clinico.js
+++ assets/js/historial-clinico.js
@@ -6,9 +6,19 @@ const planNutricional = document.getElementById("plan-nutricional");
 
 let contenidoPlanNutricional;
 
+const btnCrearInforme = document.getElementById("btn-crear-informe");
+const btnCancelarInforme = document.getElementById("btn-cancelar-informe");
+const btnGuardarInforme = document.getElementById("btn-guardar-informe");
+
+const contenedorCrearInforme = document.getElementById("contenedor-crear-informe");
+const inputPesoCrear = document.getElementById("inputPesoCrear");
+const inputTallaCrear = document.getElementById("inputTallaCrear");
+const inputCinturaCrear = document.getElementById("inputCinturaCrear");
+const textAreaCrear = document.getElementById("extraCrear");
+
 document.addEventListener('click', (e)=>{
 
-    // Evento boton editar
+    // Evento boton editar plan alimentacion
     if(e.target === btnEditar){
         planNutricional.contentEditable = "true";
         planNutricional.focus()
@@ -18,7 +28,7 @@ document.addEventListener('click', (e)=>{
         contenidoPlanNutricional = planNutricional.textContent
     }
 
-    // Evento boton cancelar
+    // Evento boton cancelar plan alimentacion
     if(e.target === btnCancelar){
         planNutricional.contentEditable = "false";
         planNutricional.textContent = contenidoPlanNutricional
@@ -27,11 +37,37 @@ document.addEventListener('click', (e)=>{
         btnGuardar.classList.toggle("hidden")
     }
 
-    // Evento boton guardar
+    // Evento boton guardar plan alimentacion
     if(e.target === btnGuardar){
         planNutricional.contentEditable = "false";
         btnEditar.classList.toggle("hidden")
         btnCancelar.classList.toggle("hidden")
         btnGuardar.classList.toggle("hidden")
     }
+
+
+
+    // Evento boton crear informe
+    if(e.target === btnCrearInforme){
+        contenedorCrearInforme.classList.remove("hidden")
+    }
+
+    // Evento boton cancelar informe
+    if(e.target === btnCancelarInforme){
+        contenedorCrearInforme.classList.add("hidden")
+        inputPesoCrear.value = "";
+        inputTallaCrear.value = "";
+        inputCinturaCrear.value = "";
+        textAreaCrear.value = "";
+    }
+
+    // Evento boton Guardar informe
+    if(e.target === btnGuardarInforme){
+        contenedorCrearInforme.classList.add("hidden")
+        inputPesoCrear.value = "";
+        inputTallaCrear.value = "";
+        inputCinturaCrear.value = "";
+        textAreaCrear.value = "";
+    }
+
 })
